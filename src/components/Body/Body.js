23a6fa32fcae9270cac756d29d2d3999037a8c24
dangkhoa <dangkhoa200399@gmail.com>
2022-07-14 21:58:06 +0700

    first commit

---
+++ src/components/Body/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Banner from './Banner'
+import Item from './Item'
+export default function Body() {
+    return (
+        <div>
+           <Banner></Banner>
+           <Item></Item>
+        </div>
+    )
+}
---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+import Body from '../components/Body/Body'
+import Footer from '../components/Footer'
+
+export default function HomePage() {
+  return (
+    <div>
+        <Header></Header>
+        <Body></Body>
+        <Footer></Footer>
+    </div>
+  )
+}
